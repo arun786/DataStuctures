ea34ea2a053232353eadbcdf5fdd280e178bc4c7
arun789 <adwiti1975@gmail.com>
2017-12-09 12:52:25 -0700

    CheckBalanceParenthesis

--- src/ass/check_brackets.java
+++ src/ass/check_brackets.java
@@ -1,61 +1,61 @@
 package ass;
 
+import java.util.Scanner;
 import java.util.Stack;
 
 public class check_brackets {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String checkBrackets = scan.next();
+        System.out.println(checkBalance(checkBrackets));
 
     }
 
     public static String checkBalance(String balance) {
 
         char[] input = balance.toCharArray();
-        Stack<Character> stack = new Stack<>();
-        Stack<Character> stack1 = new Stack<>();
+        Stack<String> stack = new Stack<>();
+        boolean flag = false;
 
         for (int i = 0; i < input.length; i++) {
-            stack1.push(input[i]);
             if (input[i] == '(' || input[i] == '{' || input[i] == '[') {
-                stack.push(input[i]);
+                stack.push( i + "," + input[i]);
+                flag = false;
             } else {
-
+                flag = true;
                 if (stack.isEmpty() && (input[i] == ')' || input[i] == '}' || input[i] == ']')) {
                     return "" + (i + 1);
                 }
 
                 if (input[i] == ')' || input[i] == '}' || input[i] == ']') {
 
-                    char popedValue = stack.pop();
+                    String pop = stack.pop();
+                    String split[] = pop.split(",");
+                    String index = split[0];
+                    char popedValue = split[1].toCharArray()[0];
                     if ((popedValue == '(' && input[i] != ')') || (popedValue == '[' && input[i] != ']')
                             || (popedValue == '{' && input[i] != '}')) {
-                        return "" + (i + 1);
+                            return "" + (i+1);
                     }
                 }
             }
         }
 
-        int index = stack1.size();
         if (!stack.isEmpty()) {
-            char firstValueNotMatched = stack.pop();
-            for (int i = stack1.size() - 1; i >= 0; i--) {
-                if (firstValueNotMatched == input[i]) {
-                    if (i > 0) {
-                        if (input[i] == input[i - 1]) {
-                            index--;
-                        }
-                    }
-                }
+            if (flag == false) {
+                String p = stack.firstElement();
+                String split[] = p.split(",");
+                String i = split[0];
+                return "" + (Integer.parseInt(i) + 1);
             }
-
-            return "" + (index);
+            if (!stack.isEmpty()) {
+                String pop = stack.pop();
+                String split[] = pop.split(",");
+                String index = split[0];
+                return "" + (Integer.parseInt(index) + 1);
+            }
         }
-
-
-        /*if (!stack.isEmpty()) {*/
-
-        /*}*/
-
         return "Success";
     }
 }
